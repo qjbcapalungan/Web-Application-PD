d104427199ec62bbcda6ab54000f5c2c2d8818f9
Damdammmmmmmm <qjbcapalungan@tip.edu.ph>
2025-03-09 10:07:35 +0800

    No Barchart left side

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/MainPage.css';
 import Sidebar from './Sidebar';
 import Dataloggers from './Dataloggers';
 import Map from './Map';
 
 function MainPage() {
+
+
+  // Make the condition true para lumabas yung barchart for dataloggers.6
+  const [showDataloggers, setShowDataloggers] = useState(false); // Initially hidden
+
   return (
     <div className="main-page">
       <Sidebar />
       <div className="main-content">
-        <Dataloggers />
+        {showDataloggers && <Dataloggers />}
         <Map />
       </div>
     </div>
